211f1f1a01af20faa23259a03edb71e1081465c2
Maxim <vaslyaev.m.m@effective-group.ru>
2019-11-18 02:19:55 +0300

    Room and Wallpaper

---
+++ src/main/java/ru/rosbank/javaschool/service/RoomService.java
@@ -0,0 +1,16 @@
+package ru.rosbank.javaschool.service;
+
+import ru.rosbank.javaschool.repository.Room;
+
+public class RoomService {
+
+    public void perimeter(Room room) {
+        if (room.getLength() > 0 && room.getWidth() > 0) {
+            room.setPerimeter(room.getLength() * 2 + room.getWidth() * 2);
+            System.out.println("Perimeter " + room.getPerimeter());
+        } else {
+            System.out.println("Incorrect parameters");
+        }
+    }
+
+}
---
+++ src/test/java/ru/rosbank/javaschool/service/WallpaperServiceTest.java
@@ -0,0 +1,41 @@
+package ru.rosbank.javaschool.service;
+
+import org.junit.jupiter.api.Test;
+import ru.rosbank.javaschool.repository.Room;
+import ru.rosbank.javaschool.repository.Wallpaper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WallpaperServiceTest {
+    private static RoomService roomService =  new RoomService();
+    private static WallpaperService wallpaperService = new WallpaperService();
+
+    private Room roomNumber1 = new Room(5,6,2.75);
+    private Wallpaper wallpaperForRoomNumber1 = new Wallpaper(1.06, 10);
+
+    private Room roomNumber2 = new Room(3,6.5,2.85);
+    private Wallpaper wallpaperForRoomNumber2 = new Wallpaper(1.06, 10, 0.6);
+
+    private Room roomNumber3 = new Room(3.3,7.9,2.87);
+    private Wallpaper wallpaperForRoomNumber3 = new Wallpaper(1.01, 10.5, 0.32, 0.16);
+
+    private Room roomNumber4 = new Room(-3.3,7.9,2.87);
+    private Wallpaper wallpaperForRoomNumber4 = new Wallpaper(1.01, 10.5, 0.32, 0.16);
+
+    private Room roomNumber5 = new Room(-3.3,7.9,2.87);
+    private Wallpaper wallpaperForRoomNumber5 = new Wallpaper(1.01, 2.5, 1.32, 3.16);
+
+    @Test
+    void all() {
+        roomService.perimeter(roomNumber1);
+        wallpaperService.initialize(roomNumber1, wallpaperForRoomNumber1);
+        roomService.perimeter(roomNumber2);
+        wallpaperService.initialize(roomNumber2, wallpaperForRoomNumber2);
+        roomService.perimeter(roomNumber3);
+        wallpaperService.initialize(roomNumber3, wallpaperForRoomNumber3);
+        roomService.perimeter(roomNumber4);
+        wallpaperService.initialize(roomNumber4, wallpaperForRoomNumber4);
+        roomService.perimeter(roomNumber5);
+        wallpaperService.initialize(roomNumber5, wallpaperForRoomNumber5);
+    }
+}
